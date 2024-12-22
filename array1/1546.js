72f0cb18984eb67b8b62dc50c7967e8fa78abbe3
yunaxong <songyoona1209@gmail.com>
2024-12-22 22:43:07 +0900

    1546 문제 - node.js

---
+++ array1/1546.js
@@ -0,0 +1,15 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+
+let N = input[0];
+let grade = input[1].split(' ').map(Number);
+let max = Math.max(...grade);
+let sum = 0.0;
+let avg = 0.0;
+
+for(var i = 0; i < grade.length; i++){
+    sum += (grade[i]/ max * 100);
+}
+avg = sum/grade.length
+console.log(avg);
+
+
