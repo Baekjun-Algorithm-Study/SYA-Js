b847e81c20d03f63d51142e7af97c97690988d5f
yunaxong <songyoona1209@gmail.com>
2025-01-05 20:43:57 +0900

    10988 문제 - node.js

--- deep1st/10988.js
+++ deep1st/10988.js
@@ -1,4 +1,19 @@
-const fs = require('fs');
-const path = require('path');
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
 
-let input = fs.readFileSync(path.join(__dirname, 'example.txt'), 'utf8').toString().split('\n');
+let word = input[0];
+let forward = "";
+let backward = "";
+
+for(var i = 0; i < word.length; i++){
+    forward+=word[i];
+}
+
+for(var i = word.length-1; i >= 0; i--){
+    backward+=word[i];
+}
+
+if(forward == backward){
+    console.log(1);
+}else{
+    console.log(0);
+}
