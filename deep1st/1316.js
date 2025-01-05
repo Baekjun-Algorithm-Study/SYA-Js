3a823744fbfa44efd930758788b26430b9db2233
yunaxong <songyoona1209@gmail.com>
2025-01-05 23:25:39 +0900

    1316 문제 - node.js

--- deep1st/1316.js
+++ deep1st/1316.js
@@ -1,4 +1,24 @@
-const fs = require('fs');
-const path = require('path');
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
 
-let input = fs.readFileSync(path.join(__dirname, 'example.txt'), 'utf8').toString().split('\n');
+let caseCount = Number(input[0]);
+let groupWordCase = 0;
+
+for(var i = 1; i <= caseCount; i++){
+    let word = input[i];
+    let testWord = [];
+    let isGroupWord = true;
+
+    for(let j = 0; j < word.length; j++){
+        if(testWord.indexOf(word[j]) == -1){
+            testWord.push(word[j]);
+        }else if(testWord.indexOf(word[j]) !== testWord.length-1) {
+            isGroupWord = false;
+            break;
+        }      
+    }
+    if(isGroupWord){
+        groupWordCase += 1;
+    }
+}
+
+console.log(groupWordCase);
