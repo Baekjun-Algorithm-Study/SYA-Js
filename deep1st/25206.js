07f56ed1364ad4d4d4b933dd0b97de55b923fcbc
yunaxong <songyoona1209@gmail.com>
2025-01-06 00:49:28 +0900

    25206 - node.js

--- deep1st/25206.js
+++ deep1st/25206.js
@@ -1,4 +1,26 @@
-const fs = require('fs');
-const path = require('path');
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+let totalScore = 0;
+let totalCredit = 0;
 
-let input = fs.readFileSync(path.join(__dirname, 'example.txt'), 'utf8').toString().split('\n');
+for(var i = 0; i < input.length; i++){
+        const[, credit, rank] = input[i].split(" ");
+        if(rank !== "P"){  
+            const numCredit = parseFloat(credit);
+            totalCredit += numCredit;
+            const ranks = {
+                "A+":4.5,
+                "A0":4.0,
+                "B+":3.5,
+                "B0":3.0,
+                "C+":2.5,
+                "C0":2.0,
+                "D+":1.5,
+                "D0":1.0,
+                "F":0.0
+            };
+            totalScore += numCredit*ranks[rank];
+        }
+}
+
+let majorScore = totalScore/totalCredit;
+console.log(majorScore.toFixed(6));
