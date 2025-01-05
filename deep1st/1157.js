01c8510bb4b882d2959716f517b81712272db1fc
yunaxong <songyoona1209@gmail.com>
2025-01-05 21:25:19 +0900

    11557 문제 - node.js

--- deep1st/1157.js
+++ deep1st/1157.js
@@ -1,4 +1,28 @@
-const fs = require('fs');
-const path = require('path');
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
 
-let input = fs.readFileSync(path.join(__dirname, 'example.txt'), 'utf8').toString().split('\n');
+let word = input[0].toLowerCase();
+let frequency = {};
+
+for(let char of word){
+    if(frequency[char]){
+        frequency[char]++;
+    }else{
+        frequency[char] = 1;
+    }
+}
+
+let max = Math.max(...Object.values(frequency));
+let maxChar;
+let maxCount = 0;
+
+for(let char in frequency){
+    if(frequency[char] == max){
+        maxCount++
+        maxChar = char.toUpperCase();
+    };
+}
+if(maxCount > 1){
+    console.log("?")
+}else{
+    console.log(maxChar);
+}
